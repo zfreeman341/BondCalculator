4f69962eddeaf5c3dcf91c9654b2389b60d1327b
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-11-06 09:59:01 -0500

    final updates

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Bond Calculator GUI
 
-This Java application allows users to calculate the price or yield of a bond using a user-friendly Swing GUI.
+This Java application allows users to calculate the price or yield of a bond with a user-friendly Swing GUI.
 
 # Prerequisites:
-  - Java (Java 8 or later).
+  - Java (Java 8 or later)
   - Maven
 
 # Step 1: Complile the Code
 
-When using the program for the first time, make sure to compile the code. From the root directory, use the following in the command line interface:
+When using the program for the first time, or after making any changes to the program make sure to compile the code. From the root directory, use the following in the command line interface:
 
   mvn clean compile
 
@@ -20,11 +20,17 @@ Once compiled, the code can be run from the root directory by typing the followi
 
   mvn exec:java
 
-# Step 3: Running Tests
+# OPTIONAL Step 3: Running Tests
 
 To run the test suite, run:
 
   mvn test
+
+# Directory Structure:
+  - The primary code for this program can be found at the file path /src/main/java/com/zfreeman341/bondCalculator.
+  - Test suites can be found at /src/test/java/com/zfreeman341/bondCalculator.
+  - This program uses Maven as its build automation tool. The configuration for this project can be found at pom.xml in the root directory.
+
 
 # Input Descriptions:
   - Coupon: The annual interest rate of the bond. This is expressed as a decimal. For example, 0.05 represents a 5% coupon rate.
@@ -34,7 +40,7 @@ To run the test suite, run:
   - Price: In 'Yield Calculator' mode, this represents the current market price of the bond.
 
 # Usage Tips
-  - Make sure to input valid numbers to avoid errors
+  - Make sure to input valid numbers to avoid errors.
   - Use the "Switch to... Calculator" button to toggle between calculating price and bond yields.
 .
 # Contact Information
--- src/main/java/com/zfreeman341/bondCalculator/BondCalculator.java
+++ src/main/java/com/zfreeman341/bondCalculator/BondCalculator.java
@@ -4,13 +4,30 @@ public class BondCalculator {
         private static final int MAX_ITERATIONS = 10000;
         private static final double TOLERANCE = 1e-7;
 
+        /**
+     * Calculates the price of a bond given the coupon rate, years to maturity, face value, and yield
+     * @param couponRate The coupon rate of the bond
+     * @param years The number of years until maturity
+     * @param face The face value of the bond
+     * @param rate The yield of the bond
+     * @return The price of the bond
+     */
     public double calcPrice(double couponRate, int years, double face, double rate) {
         double couponPayment = couponRate * face; // This will convert the coupon rate to actual payment
         return calculateBondPrice(couponPayment, years, face, rate);
     }
 
+
+        /**
+     * Calculates the yield of a bond given the coupon rate, years to maturity, face value, and market price
+     * @param couponRate The coupon rate of the bond
+     * @param years The number of years until maturity
+     * @param face The face value of the bond
+     * @param marketPrice The market price of the bond
+     * @return The yield of the bond
+         */
     public double calcYield(double couponRate, int years, double face, double marketPrice) {
-        double couponPayment = couponRate * face; // Convert the coupon rate to actual payment
+        double couponPayment = couponRate * face; // Convert the coupon rate to actual annual payment
         double lowerBound = 0.0;
         double upperBound = 1.0;
         double estimatedYield = couponRate;
@@ -34,6 +51,7 @@ public class BondCalculator {
         return estimatedYield; // If it hasn't converged in MAX_ITERATIONS, this will return the last estimate (optimization)
     }
 
+    // Calculates the actual bond price
     private  double calculateBondPrice(double coupon, int years, double face, double yield) {
         double price = 0;
         for (int i = 1; i <= years; i++) {
